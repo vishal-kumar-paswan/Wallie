1ad17bb3c110672f3239c1d0e484829710acc2b1
Vishal Kumar Paswan <kumarpaswanvishal@gmail.com>
2023-09-10 17:54:07 +0530

    Refactored splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  void routeToHomeScreen(BuildContext ctx) {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushNamed(ctx, "/homescreen");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    routeToHomeScreen(context);
+    return Scaffold(
+      body: Center(
+        child: DefaultTextStyle(
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 50.0,
+            fontFamily: GoogleFonts.pacifico().fontFamily,
+          ),
+          child: AnimatedTextKit(
+            isRepeatingAnimation: false,
+            totalRepeatCount: 1,
+            animatedTexts: [
+              TyperAnimatedText(
+                'Wallie.',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
