bf97fc4fea943b8fcf8ccb5cab37e15ce7d6bc95
Vishal Kumar Paswan <kumarpaswanvishal@gmail.com>
2023-09-10 23:42:00 +0530

    Refactored ImagesModel

--- lib/models/images_model.dart
+++ lib/models/images_model.dart
@@ -2,22 +2,25 @@ class ImagesModel {
   String? id;
   String? description;
   String? thumb;
+  String? regular;
   String? raw;
 
-  ImagesModel({this.id, this.description, this.thumb, this.raw});
+  ImagesModel({this.id, this.description, this.thumb, this.regular, this.raw});
 
   ImagesModel.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     description = json['description'];
     thumb = json['thumb'];
+    regular = json['regular'];
     raw = json['raw'];
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
+    final Map<String, dynamic> data = {};
     data['id'] = id;
     data['description'] = description;
     data['thumb'] = thumb;
+    data['regular'] = regular;
     data['raw'] = raw;
     return data;
   }
