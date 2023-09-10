b33062a84243f4d5619a2f7bb12dacdcb3984dbc
Vishal Kumar Paswan <kumarpaswanvishal@gmail.com>
2023-09-10 23:41:39 +0530

    Added /image route

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wallie/cubits/internet_cubit/internet_cubit.dart';
 import 'package:wallie/screens/home_screen.dart';
-import 'package:wallie/screens/no_internet_screen.dart';
+import 'package:wallie/screens/image_screen.dart';
+import 'package:wallie/widgets/no_internet_widget.dart';
 import 'package:wallie/screens/splash_screen.dart';
 
 class Routes {
   static Route? onGenerateRoute(RouteSettings settings) {
+    Map<String, dynamic> arguments;
+
     switch (settings.name) {
       case "/splashscreen":
         return MaterialPageRoute(
@@ -25,8 +28,18 @@ class Routes {
         return MaterialPageRoute(
           builder: (context) => const NoInternetScreen(),
         );
+
+      case "/image":
+        //fetching arguments passed while navigating as map
+        arguments = settings.arguments as Map<String, dynamic>;
+        return MaterialPageRoute(
+          builder: (context) => ImageScreen(
+            imageUrl: arguments["imageUrl"],
+            index: arguments["index"],
+          ),
+        );
       default:
+        return null;
     }
-    return null;
   }
 }
