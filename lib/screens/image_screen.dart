1d923132e89183d0a136ed2996f0bf1453f7ff84
Vishal Kumar Paswan <kumarpaswanvishal@gmail.com>
2023-09-12 23:18:14 +0530

    Added FAB and download feature

--- lib/screens/image_screen.dart
+++ lib/screens/image_screen.dart
@@ -1,8 +1,15 @@
+import 'dart:developer';
+
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_expandable_fab/flutter_expandable_fab.dart';
+import 'package:flutter_file_downloader/flutter_file_downloader.dart';
+import 'package:wallie/utils/display_snackbar.dart';
 
 class ImageScreen extends StatelessWidget {
-  const ImageScreen({
+  final _key = GlobalKey<ExpandableFabState>();
+  ImageScreen({
     super.key,
     required this.imageUrl,
     required this.index,
@@ -21,8 +28,8 @@ class ImageScreen extends StatelessWidget {
             height: double.infinity,
             width: double.infinity,
             fit: BoxFit.cover,
-            imageUrl:
-                imageUrl,   placeholder: (context, url) {
+            imageUrl: imageUrl,
+            placeholder: (context, url) {
               return const Material(
                 child: Center(
                   child: Text(
@@ -36,6 +43,61 @@ class ImageScreen extends StatelessWidget {
           ),
         ),
       ),
+      floatingActionButtonLocation: ExpandableFab.location,
+      floatingActionButton: ExpandableFab(
+        key: _key,
+        overlayStyle: ExpandableFabOverlayStyle(blur: 3),
+        childrenOffset: Offset.fromDirection(30),
+        children: [
+          FloatingActionButton(
+            onPressed: () async {
+              FileDownloader.downloadFile(
+                name: 'wallie_wallpaper.jpeg',
+                url: imageUrl,
+                onDownloadCompleted: (String path) {
+                  DisplaySnackbar(
+                    context: context,
+                    message: 'File downloaded',
+                    color: Colors.green,
+                  );
+                },
+                onDownloadError: (String error) {
+                  DisplaySnackbar(
+                    context: context,
+                    message: 'Failed to download',
+                    color: Colors.red,
+                  );
+                },
+              );
+
+              _closeFABMenu();
+            },
+            tooltip: 'Download',
+            child: const Icon(CupertinoIcons.cloud_download),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              _closeFABMenu();
+            },
+            tooltip: 'Set on lock screen',
+            child: const Icon(CupertinoIcons.lock_fill),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              _closeFABMenu();
+            },
+            tooltip: 'Set on home screen',
+            child: const Icon(CupertinoIcons.home),
+          ),
+        ],
+      ),
     );
+  }
+
+  void _closeFABMenu() {
+    final state = _key.currentState;
+    if (state != null) {
+      state.toggle();
+    }
   }
 }
